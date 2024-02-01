b8656bcf9d2b934262287aa0f376a352d8cc0daa
drhydeandmrjekyll <ronaldkevinnene@gmail.com>
2024-01-31 22:45:46 -0800

    UTF8

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+UTF-8 Validation
+"""
+
+
+def validUTF8(data):
+    """
+    Determines if given data set represents valid UTF-8 encoding
+    """
+    bytes_remaining = 0
+
+    for num in data:
+        # Check if current byte is a continuation byte
+        if bytes_remaining:
+            if num >> 6 != 0b10:
+                return False
+            bytes_remaining -= 1
+        else:
+            # Count number of leading 1s to determine byte length
+            if num >> 3 == 0b11110:
+                bytes_remaining = 3
+            elif num >> 4 == 0b1110:
+                bytes_remaining = 2
+            elif num >> 5 == 0b110:
+                bytes_remaining = 1
+            elif num >> 7:
+                return False
+
+    return bytes_remaining == 0
