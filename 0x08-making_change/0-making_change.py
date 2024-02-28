6193fb0910fa3a14c6eb0f7fbbbaa5d08f817927
drhydeandmrjekyll <ronaldkevinnene@gmail.com>
2024-02-27 22:57:40 -0800

    making_change_0

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Module for making change problem
+"""
+
+
+def makeChange(coins, total):
+    """
+    Determines fewest number of coins needed to meet a given amount total
+    Args:
+        coins (list): A list of the values of coins in your possession
+        total (int): The target amount total
+    Returns:
+        int: Fewest number of coins needed to meet total
+    """
+    if total < 1:
+        return 0
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for j in range(coin, total + 1):
+            dp[j] = min(dp[j], dp[j - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
+
+
+if __name__ == "__main__":
+    print(makeChange([1, 2, 25], 37))  # Output: 7
+    print(makeChange([1256, 54, 48, 16, 102], 1453))  # Output: -1
